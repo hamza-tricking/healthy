59a9275695080bb402ed986c7b672da5c639fd0c
Mohamed <hamzatricks@gmail.com>
2026-03-01 00:24:37 +0100

    first commit

--- routes/api/services.js
+++ routes/api/services.js
@@ -5,7 +5,7 @@ const Service = require('../../models/Service');
 // GET all services
 router.get('/', async (req, res) => {
   try {
-    const services = await Service.find({ isActive: true }).sort({ order: 1 });
+    const services = await Service.find({}).sort({ order: 1 });
     res.json(services);
   } catch (error) {
     console.error(error);
